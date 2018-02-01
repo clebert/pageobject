4f20788b1ac3147ad66a411077c1fdbb9a23a369
Clemens Akens <clebert@me.com>
2018-02-01 21:23:45 +0100

    feat: new engine api

--- @pageobject/engine/src/index.ts
+++ @pageobject/engine/src/index.ts
@@ -1,42 +1,48 @@
-/**
- * `import {execute} from '@pageobject/engine';`
- */
-export async function execute<TResult>(
+export type Engine<TResult> = (
   command: () => Promise<TResult>,
-  timeout: number
-): Promise<TResult> {
-  let error = new Error(`Timeout after ${timeout} milliseconds`);
-  let resolved = false;
+  timeout?: number
+) => Promise<TResult>;
 
-  let timeoutID: any; /* tslint:disable-line no-any */
+/**
+ * `import {createEngine} from '@pageobject/engine';`
+ */
+export function createEngine<TResult = void>(
+  defaultTimeout: number
+): Engine<TResult> {
+  return async (command, timeout = defaultTimeout) => {
+    let error = new Error(`Timeout after ${timeout} milliseconds`);
+    let resolved = false;
 
-  return Promise.race([
-    (async () => {
-      while (!resolved) {
-        try {
-          const result = await command();
+    let timeoutID: any; /* tslint:disable-line no-any */
 
-          clearTimeout(timeoutID);
+    return Promise.race([
+      (async () => {
+        while (!resolved) {
+          try {
+            const result = await command();
 
-          return result;
-        } catch (e) {
-          error = e;
+            clearTimeout(timeoutID);
+
+            return result;
+          } catch (e) {
+            error = e;
+          }
+
+          await new Promise<void>(setImmediate);
         }
 
-        await new Promise<void>(setImmediate);
-      }
+        /* istanbul ignore next */
+        throw error;
+      })(),
+      (async () => {
+        await new Promise<void>(resolve => {
+          timeoutID = setTimeout(resolve, timeout);
+        });
 
-      /* istanbul ignore next */
-      throw error;
-    })(),
-    (async () => {
-      await new Promise<void>(resolve => {
-        timeoutID = setTimeout(resolve, timeout);
-      });
+        resolved = true;
 
-      resolved = true;
-
-      throw error;
-    })()
-  ]);
+        throw error;
+      })()
+    ]);
+  };
 }
