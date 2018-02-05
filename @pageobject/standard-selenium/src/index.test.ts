bfb9775704e2ff266ada5fadb0cca071eaefe2fa
Clemens Akens <clebert@users.noreply.github.com>
2018-02-06 00:45:18 +0100

    feat: improve api (#117)

--- @pageobject/standard-selenium/src/index.test.ts
+++ @pageobject/standard-selenium/src/index.test.ts
@@ -1,6 +1,6 @@
 import 'chromedriver'; /* tslint:disable-line no-import-side-effect */
 
-import {describePageTests, url} from '@pageobject/standard-test';
+import {describePageTests, testURL} from '@pageobject/standard-test';
 import {Builder, WebDriver} from 'selenium-webdriver';
 import {SeleniumPage} from '.';
 
@@ -16,7 +16,7 @@ async function createDriver(): Promise<WebDriver> {
 let page: SeleniumPage;
 
 beforeAll(async () => {
-  page = await SeleniumPage.load(url, await createDriver());
+  page = await SeleniumPage.load(testURL, await createDriver());
 });
 
 afterAll(async () => {
