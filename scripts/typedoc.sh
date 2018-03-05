80028f45d6c5b59488f1813a11cd68684510b8c0
Clemens Akens <clebert@me.com>
2018-03-05 11:37:20 +0100

    ci: update dependencies and improve scripts

--- scripts/typedoc.sh
+++ scripts/typedoc.sh
@@ -5,12 +5,21 @@ set -e
 # IMPORTANT: If you edit this file, please validate it using ShellCheck:
 # http://www.shellcheck.net/
 
-rm -rf docs/api/
+rm -rf node_modules/@types/lodash
 
+rm -rf docs/api/flexible
 "$(yarn bin)"/typedoc --out docs/api/flexible ./@pageobject/flexible
+
+rm -rf docs/api/flexible-puppeteer
 "$(yarn bin)"/typedoc --out docs/api/flexible-puppeteer ./@pageobject/flexible-puppeteer
+
+rm -rf docs/api/flexible-selenium
 "$(yarn bin)"/typedoc --out docs/api/flexible-selenium ./@pageobject/flexible-selenium
+
+rm -rf docs/api/reliable
 "$(yarn bin)"/typedoc --out docs/api/reliable ./@pageobject/reliable
+
+rm -rf docs/api/stable
 "$(yarn bin)"/typedoc --out docs/api/stable ./@pageobject/stable
 
 "$(yarn bin)"/replace-in-file '/Defined in .+node_modules./g' 'Defined in ' 'docs/**/*.html' --isRegex --verbose
