ecd57b26e0fcd6b04c171cde9aa0b4cd38caa832
Clemens Akens <clebert@users.noreply.github.com>
2018-03-05 23:17:31 +0100

    test: fix #151 chromedriver bug (#154)

--- @pageobject/flexible-selenium/src/SeleniumAdapter.test.ts
+++ @pageobject/flexible-selenium/src/SeleniumAdapter.test.ts
@@ -1,12 +1,15 @@
-import 'chromedriver'; /* tslint:disable-line no-import-side-effect */
-
 import {FlexibleAdapterTestSuite} from '@pageobject/flexible';
 import {SeleniumAdapter} from '.';
+import {ChromeDriver} from './ChromeDriver';
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
 describe('SeleniumAdapter', () => {
   it('should pass the FlexibleAdapterTestSuite successfully', async () => {
+    const chromeDriver = new ChromeDriver();
+
+    await chromeDriver.start(4445);
+
     const adapter = await SeleniumAdapter.create({
       browserName: 'chrome',
       chromeOptions: {args: ['headless', 'disable-gpu']}
@@ -18,6 +21,8 @@ describe('SeleniumAdapter', () => {
       await testSuite.testAll();
     } finally {
       await adapter.driver.quit();
+
+      chromeDriver.stop();
     }
   });
 });
