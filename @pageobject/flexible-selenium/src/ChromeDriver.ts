1f216df2f1120b71fcf068f7b04851590acf0f27
Clemens Akens <clebert@users.noreply.github.com>
2018-03-09 09:47:38 +0100

    test(flexible-selenium): use webdriver-manager instead of chromedriver (#163)

--- @pageobject/flexible-selenium/src/ChromeDriver.ts
+++ @pageobject/flexible-selenium/src/ChromeDriver.ts
@@ -1,14 +1,15 @@
 import {ChildProcess, spawn} from 'child_process';
-import {join} from 'path';
+
+const updateConfig = require('webdriver-manager/selenium/update-config.json');
 
 export class ChromeDriver {
   private _childProcess: ChildProcess | undefined;
 
   public async start(port: number, urlBase: string = '/wd/hub'): Promise<void> {
-    const childProcess = (this._childProcess = spawn(
-      join(require.resolve('chromedriver'), '../chromedriver/chromedriver'),
-      [`--url-base=${urlBase}`, `--port=${port}`]
-    ));
+    const childProcess = (this._childProcess = spawn(updateConfig.chrome.last, [
+      `--url-base=${urlBase}`,
+      `--port=${port}`
+    ]));
 
     process.env.SELENIUM_REMOTE_URL = `http://localhost:${port}${urlBase}`;
 
