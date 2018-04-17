ddf092e929a43511777e416032a635bb506f2dab
Clemens Akens <clebert@me.com>
2018-04-17 23:44:23 +0200

    build(all): add support for dependabot commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,6 @@ module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
     'header-max-length': [2, 'always', 140],
-    'scope-empty': [2, 'never'],
     'scope-enum': () => [2, 'always', [...getPackages(), 'all']],
     'subject-case': [2, 'always', ['lower-case', 'kebab-case']]
   }
